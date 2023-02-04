567a277fe8bd5bb9f24bf915b04045ab47804cb7
thombator <thom.bator@gmail.com>
2023-02-04 12:07:07 -0500

    changed media query to improve appearance on small screens

---
+++ index.js
@@ -0,0 +1,8 @@
+const pgCounter = document.getElementById('pageCounter');
+const dummyNum = 100
+
+pgCounter.innerHTML =
+    `
+    <h3>Site Visitors: ${dummyNum} || Page Hits: ${dummyNum}</h3> 
+    `
+;
